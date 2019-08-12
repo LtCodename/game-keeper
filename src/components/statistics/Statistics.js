5c3d605e0c6a7e2883e811eb63b7f0c87a99c2a6
Yevhen Chernenko <chernenko.eu@gmail.com>
2019-08-12 20:37:50 +0300

    New stats

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -13,22 +13,85 @@ class Statistics extends React.Component {
   render() {
     let sectionsNumber = 0;
     let gamesNumber = 0;
+    let years = [];
+
+    let onPC = 0;
+    let gameWordPC = "games";
+    let onSwitch = 0;
+    let gameWordSwitch = "games";
+    let onPS4 = 0;
+    let gameWordPS4 = "games";
+    let onXbox = 0;
+    let gameWordXbox = "games";
+    let iOS = 0;
+    let gameWordiOS = "games";
 
     for (var section in this.props.data) {
-      //console.log(this.props.data[section].content);
+      //How many sections
       sectionsNumber += this.props.data[section].content.length;
 
       for (var prop in this.props.data[section].content) {
-        //console.log(this.props.data[section].content[prop].games);
+        //How many games
         gamesNumber += this.props.data[section].content[prop].games.length;
+
+        for (var game in this.props.data[section].content[prop].games) {
+
+          //Unique years
+          let year = this.props.data[section].content[prop].games[game].releaseDate.substring(0,4);
+
+          if (years.indexOf(year) === -1) {
+            years.push(year);
+          }
+
+          years.sort((a, b) => {
+
+            if (a < b) {
+              return -1;
+            }
+            if (a > b) {
+              return 1;
+            }
+            return 0;
+          });
+
+          //How many platforms
+          for (var platform in this.props.data[section].content[prop].games[game].platforms) {
+            switch (this.props.data[section].content[prop].games[game].platforms[platform].name) {
+              case "PC":
+                onPC++;
+                (onPC === 1) ? gameWordPC = "game" : gameWordPC = "games";
+                break;
+              case "Nintendo Switch":
+                onSwitch++;
+                (onSwitch === 1) ? gameWordSwitch = "game" : gameWordSwitch = "games";
+                break;
+              case "PlayStation 4":
+                onPS4++;
+                (onPS4 === 1) ? gameWordPS4 = "game" : gameWordPS4 = "games";
+                break;
+              case "iOS":
+                iOS++;
+                (iOS === 1) ? gameWordiOS = "game" : gameWordiOS = "games";
+                break;
+              case "Xbox":
+                onXbox++;
+                (onXbox === 1) ? gameWordXbox = "game" : gameWordXbox = "games";
+                break;
+              default:
+            }
+          }
+        }
       }
     }
 
     return (
       <div className="statistics">
-        <p className="dashboardText">{`You have ${this.props.data.length} lists.`}</p>
-        <p className="dashboardText">{`You have ${sectionsNumber} sections in your lists.`}</p>
-        <p className="dashboardText">{`You have ${gamesNumber} games in your sections.`}</p>
+        <p className="dashboardText">{`There are ${this.props.data.length} lists in your Game Keeper.`}</p>
+        <p className="dashboardText">{`There are ${sectionsNumber} sections in those lists.`}</p>
+        <p className="dashboardText">{`There are ${gamesNumber} games in those sections.`}</p>
+        <p className="dashboardText">{`There are ${onPC} PC ${gameWordPC}, ${onSwitch} Nintendo Switch ${gameWordSwitch},
+          ${onPS4} ${gameWordPS4} for PlayStation 4, ${iOS} ${gameWordiOS} for iOS and ${onXbox} Xbox ${gameWordXbox}.`}</p>
+        <p className="dashboardText">{`Your Game Keeper represents games released in ${years.length} different years! From ${years[0]} to ${years[years.length - 1]}`}</p>
       </div>
     )
   }
