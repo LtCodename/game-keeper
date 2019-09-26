2bdb6ab433a550a123af6102cbf118a98115f7d7
Yevhen Chernenko <chernenko.eu@gmail.com>
2019-09-27 01:33:32 +0300

    Warnings before deleting/adding developers

--- src/components/developers/Developers.js
+++ src/components/developers/Developers.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import './Developers.css';
 import { connect } from 'react-redux'
+import WarningModalWindow from '../warning-modal-window/WarningModalWindow.js';
 declare var firebase;
+declare var $;
 
 class Developers extends React.Component {
   constructor(props) {
     super(props);
 
     this.deleteItem = this.deleteItem.bind(this);
+    this.onDeleteItem = this.onDeleteItem.bind(this);
+    this.openModalWarningWindow = this.openModalWarningWindow.bind(this);
+    this.resetState = this.resetState.bind(this);
 
     this.state = {
+      idToDelete: "",
+      showModalWindow: false
     };
   }
 
-  deleteItem(id) {
-    firebase.firestore().collection('developers').doc(id).delete();
+  deleteItem() {
+    firebase.firestore().collection('developers').doc(this.state.idToDelete).delete();
+    this.setState({
+      idToDelete: ""
+    });
+  }
+
+  onDeleteItem(id) {
+    this.setState({
+      idToDelete: id
+    },() => {
+        this.openModalWarningWindow();
+      }
+    );
+  }
+
+  openModalWarningWindow() {
+    this.setState({
+      showModalWindow: true
+    }, () => {
+      $("#modalWarning").modal('show');
+      $("#modalWarning").on('hidden.bs.modal', this.resetState);
+    });
+  }
+
+  componentWillUnmount() {
+    $("#modalWarning").unbind('hidden.bs.modal');
+  }
+
+  resetState() {
+    this.setState({
+      showModalWindow: false,
+      idToDelete: ""
+    });
   }
 
   render() {
@@ -22,17 +61,30 @@ class Developers extends React.Component {
       return (
         <div className="developersBlock" key={elem.id} value={elem.id}>
           {elem.name}
-          <button className="btn deleteButton" onClick={() => this.deleteItem(elem.id)}><img className="deleteIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-delete-developer.svg'}></img></button>
+          <button className="btn actionButton" onClick={() => this.onDeleteItem(elem.id)}>
+            <img className="deleteIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-delete-developer.svg'}></img>
+          </button>
+          <button className="btn actionButton">
+            <img className="deleteIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-edit-developer.svg'}></img>
+          </button>
         </div>
       );
     });
 
+    const modalWarningWindow = (
+      <WarningModalWindow
+        onProceed={this.deleteItem}
+        message={`Are you sure you want to delete this item?`} />
+    );
+
     return (
       <div className="developersWrapper">
-        <p>Press delete button to remove developer from Firestore. Items are sorted alphabetically.</p>
+        <p>Edit or delete developers from database. Items are sorted alphabetically.</p>
         <div className="blocksWrapper">
           {developersToRender}
         </div>
+
+        {this.state.showModalWindow ? modalWarningWindow : ""}
       </div>
     )
   }
--- src/components/suggested/Suggested.js
+++ src/components/suggested/Suggested.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Suggested.css';
 import { connect } from 'react-redux'
+import WarningModalWindow from '../warning-modal-window/WarningModalWindow.js';
 declare var firebase;
+declare var $;
 
 class Suggested extends React.Component {
   constructor(props) {
@@ -9,21 +11,87 @@ class Suggested extends React.Component {
 
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onDeleteItem = this.onDeleteItem.bind(this);
+    this.openModalDeleteWindow = this.openModalDeleteWindow.bind(this);
+    this.openModalAddWindow = this.openModalAddWindow.bind(this);
+    this.resetState = this.resetState.bind(this);
 
     this.state = {
+      idToDelete: "",
+      nameToAdd: "",
+      showDeleteModalWindow: false,
+      showAddModalWindow: false
     };
   }
 
-  deleteItem(id) {
-    firebase.firestore().collection('suggestedDevelopers').doc(id).delete();
+  onDeleteItem(id) {
+    this.setState({
+      idToDelete: id
+    },() => {
+        console.log(this.state.idToDelete);
+        this.openModalDeleteWindow();
+      }
+    );
   }
 
-  addItem(name, id) {
+  deleteItem() {
+    firebase.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
+      this.setState({
+        idToDelete: ""
+      });
+    });
+  }
+
+  openModalDeleteWindow() {
+    this.setState({
+      showDeleteModalWindow: true
+    }, () => {
+      $("#modalWarning").modal('show');
+      $("#modalWarning").on('hidden.bs.modal', this.resetState);
+    });
+  }
+
+  componentWillUnmount() {
+    $("#modalWarning").unbind('hidden.bs.modal');
+  }
+
+  resetState() {
+    this.setState({
+      showDeleteModalWindow: false,
+      showAddModalWindow: false
+    });
+  }
+
+  onAddItem(name, id) {
+    this.setState({
+      idToDelete: id,
+      nameToAdd: name
+    },() => {
+        this.openModalAddWindow();
+      }
+    );
+  }
+
+  openModalAddWindow() {
+    this.setState({
+      showAddModalWindow: true
+    }, () => {
+      $("#modalWarning").modal('show');
+      $("#modalWarning").on('hidden.bs.modal', this.resetState);
+    });
+  }
+
+  addItem() {
     firebase.firestore().collection('developers').add({
-      name: name
+      name: this.state.nameToAdd
     }).then(() => {
       firebase.firestore().collection('developers').get().then(snapshot => {
-        firebase.firestore().collection('suggestedDevelopers').doc(id).delete();
+        firebase.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
+          this.setState({
+            idToDelete: "",
+            nameToAdd: ""
+          });
+        });
       }).catch(error => {
         console.log(error.message);
       });
@@ -35,15 +103,27 @@ class Suggested extends React.Component {
       return (
         <div className="developersBlock" key={elem.id} value={elem.id}>
           {elem.name}
-          <button className="btn actionButton" onClick={() => this.deleteItem(elem.id)}>
+          <button className="btn actionButton" onClick={() => this.onDeleteItem(elem.id)}>
             <img className="actionIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-delete-developer.svg'}></img>
           </button>
-          <button className="btn actionButton" onClick={() => this.addItem(elem.name, elem.id)}>
+          <button className="btn actionButton" onClick={() => this.onAddItem(elem.name, elem.id)}>
             <img className="actionIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-add-developer.svg'}></img>
           </button>
         </div>
       );
     });
+
+    const modalDeleteWindow = (
+      <WarningModalWindow
+        onProceed={this.deleteItem}
+        message={`Are you sure you want to delete this item?`} />
+    );
+
+    const modalAddWindow = (
+      <WarningModalWindow
+        onProceed={this.addItem}
+        message={`Are you sure you want to add this item?`} />
+    );
 
     return (
       <div className="suggestedWrapper">
@@ -51,6 +131,8 @@ class Suggested extends React.Component {
         <div className="blocksWrapper">
           {developersToRender}
         </div>
+        {this.state.showDeleteModalWindow ? modalDeleteWindow : ""}
+        {this.state.showAddModalWindow ? modalAddWindow : ""}
       </div>
     )
   }
