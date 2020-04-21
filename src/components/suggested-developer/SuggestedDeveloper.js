254338cacad9c3e09326530143045f9e48b0fde3
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-04-21 14:32:24 -0700

    New modal window behaviour Part 3

---
+++ src/components/suggested-developer/SuggestedDeveloper.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import './SuggestedDeveloper.css';
+import WarningModalWindow from "../warning-modal-window/WarningModalWindow";
+import EditNameModalWindow from "../edit-name-modal-window/EditNameModalWindow";
+import fire from "../../Firebase";
+
+class SuggestedDeveloper extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            idToDelete: "",
+            nameToAdd: "",
+            idToEdit: "",
+            oldName: this.props.devData.name,
+            showDeleteModalWindow: false,
+            showAddModalWindow: false,
+            showEditWindow: false
+        };
+    }
+
+    openEditWindow = () => {
+        this.setState({
+            showEditWindow: true
+        });
+    };
+
+    onEditItem = (id, oldName) => {
+        this.setState({
+                idToEdit: id,
+                oldName: oldName
+            },() => {
+                this.openEditWindow();
+            }
+        );
+    };
+
+    editItem = (newName, id) => {
+        fire.firestore().collection('suggestedDevelopers').doc(id).update({
+            name: newName
+        }).then(() => {
+            this.resetState();
+        }).catch(error => {
+            console.log(error.message);
+        });
+    };
+
+    openModalDeleteWindow = () => {
+        this.setState({
+            showDeleteModalWindow: true
+        });
+    };
+
+    onDeleteItem = (id) => {
+        this.setState({
+                idToDelete: id
+            },() => {
+                this.openModalDeleteWindow();
+            }
+        );
+    };
+
+    deleteItem = () => {
+        fire.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
+            this.setState({
+                idToDelete: ""
+            });
+        });
+    };
+
+    resetState = () => {
+        this.setState({
+            showDeleteModalWindow: false,
+            showAddModalWindow: false,
+            showEditWindow: false,
+            idToEdit: "",
+            oldName: ""
+        });
+    };
+
+    openModalAddWindow = () => {
+        this.setState({
+            showAddModalWindow: true
+        });
+    };
+
+    onAddItem = (name, id) => {
+        this.setState({
+                idToDelete: id,
+                nameToAdd: name
+            },() => {
+                this.openModalAddWindow();
+            }
+        );
+    };
+
+    addItem = () => {
+        fire.firestore().collection('developers').add({
+            name: this.state.nameToAdd
+        }).then(() => {
+            fire.firestore().collection('developers').get().then(snapshot => {
+                fire.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
+                    this.setState({
+                        idToDelete: "",
+                        nameToAdd: ""
+                    });
+                });
+            }).catch(error => {
+                console.log(error.message);
+            });
+        });
+    };
+
+    render() {
+        const modalDeleteWindow = (
+            <WarningModalWindow
+                onProceed={this.deleteItem}
+                message={`Are you sure you want to delete this item?`}
+                show={this.state.showDeleteModalWindow}
+                hideWindow={this.resetState.bind(this)}/>
+        );
+
+        const modalAddWindow = (
+            <WarningModalWindow
+                onProceed={this.addItem}
+                message={`Are you sure you want to add this item?`}
+                show={this.state.showAddModalWindow}
+                hideWindow={this.resetState.bind(this)}/>
+        );
+
+        const modalEditWindow = (
+            <EditNameModalWindow
+                onProceed={(newName) => this.editItem(newName, this.state.idToEdit)}
+                oldName={this.state.oldName}
+                show={this.state.showEditWindow}
+                hideWindow={this.resetState.bind(this)}/>
+        );
+
+        return (
+            <>
+                <div className="developers-block">
+                    {this.props.devData.name}
+                    <button className="developer-action-button" onClick={() => this.onDeleteItem(this.props.devData.id)}>
+                        <img className="dev-action-icon" alt="" src={process.env.PUBLIC_URL + '/icons/action-delete-developer.svg'}/>
+                    </button>
+                    <button className="developer-action-button" onClick={() => this.onAddItem(this.props.devData.name, this.props.devData.id)}>
+                        <img className="dev-action-icon" alt="" src={process.env.PUBLIC_URL + '/icons/action-add-developer.svg'}/>
+                    </button>
+                    <button className="developer-action-button">
+                        <img className="dev-action-icon" onClick={() => this.onEditItem(this.props.devData.id, this.props.devData.name)} alt="" src={process.env.PUBLIC_URL + '/icons/action-edit-developer.svg'}/>
+                    </button>
+                </div>
+                {modalDeleteWindow}
+                {modalAddWindow}
+                {modalEditWindow}
+            </>
+        )
+    }
+}
+export default SuggestedDeveloper;
--- src/components/suggested/Suggested.js
+++ src/components/suggested/Suggested.js
@@ -1,176 +1,29 @@
 import React from 'react';
 import './Suggested.css';
 import { connect } from 'react-redux'
-import WarningModalWindow from '../warning-modal-window/WarningModalWindow.js';
-import EditNameModalWindow from '../edit-name-modal-window/EditNameModalWindow.js';
-import fire from "../../Firebase";
-declare var $;
+import SuggestedDeveloper from "../suggested-developer/SuggestedDeveloper";
 
 class Suggested extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      idToDelete: "",
-      nameToAdd: "",
-      idToEdit: "",
-      oldName: "",
-      showDeleteModalWindow: false,
-      showAddModalWindow: false,
-      showEditWindow: false
     };
   }
-
-  openEditWindow = () => {
-    this.setState({
-      showEditWindow: true
-    }, () => {
-      $("#editNameWindow").modal('show');
-      $("#editNameWindow").on('hidden.bs.modal', this.resetState);
-    });
-  };
-
-  onEditItem = (id, oldName) => {
-    this.setState({
-      idToEdit: id,
-      oldName: oldName
-    },() => {
-        this.openEditWindow();
-      }
-    );
-  };
-
-  editItem = (newName, id) => {
-    fire.firestore().collection('suggestedDevelopers').doc(id).update({
-      name: newName
-    }).then(() => {
-      this.resetState();
-    }).catch(error => {
-      console.log(error.message);
-    });
-  };
-
-  openModalDeleteWindow = () => {
-    this.setState({
-      showDeleteModalWindow: true
-    }, () => {
-      $("#modalWarning").modal('show');
-      $("#modalWarning").on('hidden.bs.modal', this.resetState);
-    });
-  };
-
-  onDeleteItem = (id) => {
-    this.setState({
-      idToDelete: id
-    },() => {
-        this.openModalDeleteWindow();
-      }
-    );
-  };
-
-  deleteItem = () => {
-    fire.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
-      this.setState({
-        idToDelete: ""
-      });
-    });
-  };
-
-  componentWillUnmount() {
-    $("#modalWarning").unbind('hidden.bs.modal');
-    $("#editNameWindow").unbind('hidden.bs.modal');
-  }
-
-  resetState = () => {
-    this.setState({
-      showDeleteModalWindow: false,
-      showAddModalWindow: false,
-      showEditWindow: false,
-      idToEdit: "",
-      oldName: ""
-    });
-  };
-
-  openModalAddWindow = () => {
-    this.setState({
-      showAddModalWindow: true
-    }, () => {
-      $("#modalWarning").modal('show');
-      $("#modalWarning").on('hidden.bs.modal', this.resetState);
-    });
-  };
-
-  onAddItem = (name, id) => {
-    this.setState({
-      idToDelete: id,
-      nameToAdd: name
-    },() => {
-        this.openModalAddWindow();
-      }
-    );
-  };
-
-  addItem = () => {
-    fire.firestore().collection('developers').add({
-      name: this.state.nameToAdd
-    }).then(() => {
-      fire.firestore().collection('developers').get().then(snapshot => {
-        fire.firestore().collection('suggestedDevelopers').doc(this.state.idToDelete).delete().then(() => {
-          this.setState({
-            idToDelete: "",
-            nameToAdd: ""
-          });
-        });
-      }).catch(error => {
-        console.log(error.message);
-      });
-    });
-  };
 
   render() {
     const developersToRender = this.props.suggestedDevelopers.map((elem, index) => {
       return (
-        <div className="developersBlock" key={elem.id} value={elem.id}>
-          {elem.name}
-          <button className="btn actionButton" onClick={() => this.onDeleteItem(elem.id)}>
-            <img className="actionIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-delete-developer.svg'}/>
-          </button>
-          <button className="btn actionButton" onClick={() => this.onAddItem(elem.name, elem.id)}>
-            <img className="actionIcon" alt="" src={process.env.PUBLIC_URL + '/icons/action-add-developer.svg'}/>
-          </button>
-          <button className="btn actionButton">
-            <img className="deleteIcon" onClick={() => this.onEditItem(elem.id, elem.name)} alt="" src={process.env.PUBLIC_URL + '/icons/action-edit-developer.svg'}/>
-          </button>
-        </div>
+        <SuggestedDeveloper key={index} devData={elem}/>
       );
     });
 
-    const modalDeleteWindow = (
-      <WarningModalWindow
-        onProceed={this.deleteItem}
-        message={`Are you sure you want to delete this item?`} />
-    );
-
-    const modalAddWindow = (
-      <WarningModalWindow
-        onProceed={this.addItem}
-        message={`Are you sure you want to add this item?`} />
-    );
-
-    const modalEditWindow = (
-      <EditNameModalWindow
-        onProceed={(newName) => this.editItem(newName, this.state.idToEdit)} oldName={this.state.oldName} />
-    );
-
     return (
       <div className="suggestedWrapper">
-        <p className="suggestedWrapperParagraph">Press add or delete button to add or remove developer from Firestore. Items are sorted alphabetically.</p>
+        <span className="developers-info">Press add or delete button to add or remove developer from Firestore. Items are sorted alphabetically.</span>
         <div className="blocksWrapper">
           {developersToRender}
         </div>
-        {this.state.showDeleteModalWindow ? modalDeleteWindow : ""}
-        {this.state.showAddModalWindow ? modalAddWindow : ""}
-        {this.state.showEditWindow ? modalEditWindow : ""}
       </div>
     )
   }
