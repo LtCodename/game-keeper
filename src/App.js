ef05b0d66feeef731e643632cadf60f4d44a115d
YEVHEN CHERNENKO <chernenko.eu@gmail.com>
2021-08-21 10:39:08 +0300

    test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ import Register from "./components/authentication/Register";
 export const DemoUser = "ltcodename92@gmail.com";
 export const DemoPassword = "22121992";
 export const VERSION = "1.4";
-export const TEST = "TEST";
 
 
 export const GameKeeperContext = createContext({});
